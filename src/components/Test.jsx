b3b5e33f6dcc2ed1b3a251bea00be1d210c19762
Evgeniy Shygartsov <evgeniy@MacBook-Air.local>
2022-08-14 23:47:01 +0300

    some tests

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import { flushSync } from 'react-dom'
 
 export const Test = () => {
-  console.log('hello')
+  const [number, setNumber] = useState(1)
+  const [flag, setFlag] = useState(false)
 
+  const cb = useCallback(() => {
+    // eslint-disable-next-line no-promise-executor-return
+    new Promise((r) => setTimeout(r, 100)).then(() => {
+      flushSync(() => {
+        setNumber((n) => n + 1)
+      })
+      flushSync(() => {
+        setFlag((p) => !p)
+      })
+    })
+  }, [])
+
+  console.log(number, flag)
   return (
     <div>
       Test component
+      {number}
+      <button type="button" onClick={cb}>Click me</button>
     </div>
   )
 }
